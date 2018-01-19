927b4ebb7146a0f00622c315566fabae9d3fa51f
Scott Preston <me@scottpreston.org>
2018-01-19 14:52:51 -0500

    updating git

--- 2-angular-part1/1-hello/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: `<div>hello {{name}}</div>`
-})
-export class AppComponent { name = 'world'; }
--- 2-angular-part1/1-hello/app2/main2.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule2 } from './app2.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule2);
