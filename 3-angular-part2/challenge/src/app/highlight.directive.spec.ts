b20347a5f8cb5701ef8224962cc533c2ba05cf65
Scott Preston <me@scottpreston.org>
2018-02-01 09:43:47 -0500

    fixing test

--- 3-angular-part2/challenge/src/app/highlight.directive.spec.ts
+++ 3-angular-part2/challenge/src/app/highlight.directive.spec.ts
@@ -2,6 +2,7 @@ import { HighlightDirective } from './highlight.directive';
 
 describe('HighlightDirective', () => {
   it('should create an instance', () => {
+    expect(false).toBeTruthy();
     // const directive = new HighlightDirective();
     // expect(directive).toBeTruthy();
   });
